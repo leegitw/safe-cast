0933b0f5bb269b88e16b74e7af3178b467a7daa7
Jeff Tanner <jefft@tune.com>
2017-03-07 15:05:30 -0800

    [MVI-1135] (safecast) initial

---
+++ safecast/__init__.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  @copyright 2017 TUNE, Inc. (http://www.tune.com)
+#  @namespace requests_mv_integrations
+
+__title__ = 'requests-mv-integrations'
+__version__ = '0.00.1'
+__build__ = 0x000001
+__version_info__ = tuple(__version__.split('.'))
+
+__author__ = 'jefft@tune.com'
+__license__ = 'MIT License'
+
+__python_required_version__ = (3, 0)
+
+
+def safe_cast(val, to_type, default=None):
+    """Safely cast value to type, and if failed, returned default.
+
+    Args:
+        val:
+        to_type:
+        default:
+
+    Returns:
+
+    """
+    if val is None:
+        return default
+
+    try:
+        return to_type(val)
+    except ValueError:
+        return default
+
+
+def safe_str(val):
+    """Safely cast value to str
+
+    Args:
+        val:
+
+    Returns:
+
+    """
+    return safe_cast(val, str, "")
+
+
+def safe_float(val, ndigits=2):
+    """Safely cast value to float
+
+    Args:
+        val:
+
+    Returns:
+
+    """
+    return round(safe_cast(val, float, 0.0), ndigits)
+
+
+def safe_int(val):
+    """Safely cast value to int
+
+    Args:
+        val:
+
+    Returns:
+
+    """
+    return safe_cast(safe_float(val, 0), int, 0)
+
+
+def safe_dict(val):
+    """Safely cast value to dict
+
+    Args:
+        val:
+
+    Returns:
+
+    """
+    return safe_cast(val, dict, {})
